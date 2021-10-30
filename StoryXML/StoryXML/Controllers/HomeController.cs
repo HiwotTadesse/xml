028fe49f0708378a191185e9750b1a4156acb5ea
HiwotTadesse <hiwottadese78@gmail.com>
2021-10-30 11:11:32 -0700

    xml

---
+++ StoryXML/StoryXML/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace StoryXML.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Create()
+        {
+            XmlDocument XmlDoc = new XmlDocument();
+            XmlDoc.Load(Server.MapPath("../Story.xml"));
+            XmlNode RootNode = XmlDoc.SelectSingleNode("contacts");
+
+            int storyNumber = RootNode.ChildNodes.Count;
+            XmlNode contactNode = RootNode.AppendChild(XmlDoc.CreateNode(XmlNodeType.Element, "Story", ""));
+
+            contactNode.AppendChild(XmlDoc.CreateNode(XmlNodeType.Element, "Id", "")).InnerText = storyNumber.ToString();
+            contactNode.AppendChild(XmlDoc.CreateNode(XmlNodeType.Element, "Name", "")).InnerText = contact.name;
+
+
+            XmlDoc.Save(Server.MapPath("../Story.xml"));
+
+
+            return RedirectToAction("AllStory");
+            
+        }
+
+        public ActionResult AllStory()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ StoryXML/StoryXML/Models/StoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoryXML.Models
+{
+    public class StoryModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string content { get; set; }
+    }
+}
